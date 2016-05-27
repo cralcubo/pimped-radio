ce21dbf116b50c9e8a65f65cfab4b76aae05add0
Christian Roman <cralcubo@hotmail.com>
2016-05-27 16:28:02 +0200

    - Fixed escape of regEx special characters.

--- src/main/java/bo/roman/radio/utilities/RegExUtil.java
+++ src/main/java/bo/roman/radio/utilities/RegExUtil.java
@@ -3,11 +3,12 @@ package bo.roman.radio.utilities;
 public class RegExUtil {
 	private final static String BEGINSWITH_TEMPL = "^\\b%s\\b.*$";
 	private final static String CONTAINSWORD_TEMPL = "^.*\\b%s\\b.*$";
+	private static final String SPECIALCHARS_REGEX = "([\\\\\\.\\[\\{\\(\\*\\+\\?\\^\\$\\|])";
 	
 	private final String toMatch;
 	
 	private RegExUtil(String toMatch) {
-		this.toMatch = toMatch;
+		this.toMatch = escSpecChars(toMatch);
 	}
 	
 	public static RegExUtil phrase(String word) {
@@ -15,14 +16,21 @@ public class RegExUtil {
 	}
 	
 	public boolean beginsWith(String word) {
+		word = escSpecChars(word);
 		return toMatch.matches(String.format(BEGINSWITH_TEMPL, word));
 	}
 	
 	public boolean beginsWithIgnoreCase(String word) {
+		word = escSpecChars(word);
 		return toMatch.toLowerCase().matches(String.format(BEGINSWITH_TEMPL, word.toLowerCase()));
 	}
 	
 	public boolean containsIgnoreCase(String word) {
+		word = escSpecChars(word);
 		return toMatch.toLowerCase().matches(String.format(CONTAINSWORD_TEMPL, word.toLowerCase()));
+	}
+	
+	private String escSpecChars(String val){
+		return val.replaceAll(SPECIALCHARS_REGEX, "\\\\$1");
 	}
 }
